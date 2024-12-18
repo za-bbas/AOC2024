664bd5d71a91ad4640b20cdc46605d4d4cf5bdde
Zaid Abbas <zaid.sarmad2006@gmail.com>
2024-12-17 16:03:30 -0800

    Slight optimization

--- Day6.py
+++ Day6.py
@@ -10,12 +10,12 @@ for i in range(len(lines)):
         if lines[i][j] == "^":
             point = (i,j)
             break
+path = set()
 
 def partOne():
     r, c = point
     d = 0
-    visited = set()
-    visited.add((r,c))
+    path.add((r,c))
     while 0<=r<len(lines) and 0<=c<len(lines[0]):
         nextR, nextC= r+directions[d][0], c+directions[d][1]
         if not (0<=nextR<len(lines) and 0<=nextC<len(lines[0])): break
@@ -24,37 +24,35 @@ def partOne():
         else:
             r = nextR
             c = nextC
-        if (r,c) not in visited:
-            visited.add((r,c))
-    print(len(visited))
+        if (r,c) not in path:
+            path.add((r,c))
+    print(len(path))
 
 def partTwo():
     visited = set()
     valid = set()
     d = 0 
     r, c = point
-    for i in range(len(lines)):
-        for j in range(len(lines)):
-            r, c = point
-            d = 0  # Reset direction
-            visited = set()  # Reset visited
-            # we will pretend that there is an object there, not actually change the map
-            if i == r and j == c: continue
-            while 0<=r<len(lines) and 0<=c<len(lines[0]):
-                nextR, nextC= r+directions[d][0], c+directions[d][1]
-                if not (0<=nextR<len(lines) and 0<=nextC<len(lines[0])): break
-                if lines[nextR][nextC] == "#" or (nextR == i and nextC == j):
-                    d = (d + 1) % 4
-                else:
-                    r = nextR
-                    c = nextC
-                if (r,c,d) not in visited:
-                    visited.add((r,c,d))
-                else:
-                    valid.add((i,j))
-                    visited.clear()
-                    break
+    for i, j in path:
+        r, c = point
+        d = 0  # Reset direction
+        visited = set()  # Reset visited
+        if i == r and j == c: continue
+        while 0<=r<len(lines) and 0<=c<len(lines[0]):
+            nextR, nextC= r+directions[d][0], c+directions[d][1]
+            if not (0<=nextR<len(lines) and 0<=nextC<len(lines[0])): break
+            if lines[nextR][nextC] == "#" or (nextR == i and nextC == j):
+                d = (d + 1) % 4
+            else:
+                r = nextR
+                c = nextC
+            if (r,c,d) not in visited:
+                visited.add((r,c,d))
+            else:
+                valid.add((i,j))
+                visited.clear()
+                break
     print(len(valid))
 
 partOne()
-partTwo() #nearly a minute to run lol
+partTwo() # cut down from 1 min to 10 seconds
