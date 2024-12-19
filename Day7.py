0d7ebcc1ebd7fdc1a4d7bf81015e1c0181026c24
Zaid Abbas <zaid.sarmad2006@gmail.com>
2024-12-18 17:16:02 -0800

    parts 1 & 2

--- Day7.py
+++ Day7.py
@@ -1,15 +1,36 @@
 lines = [line.strip() for line in open("input7.txt", "r")]
 
 def partOne():
-    print(sum(int(line.split(":")[0]) for line in lines if check(int(line.split(":")[0]), line.split(":")[1]) ))
+    print(sum(int(line.split(":")[0]) for line in lines if check(int(line.split(":")[0]), line.split(":")[1])))
 
 def check(num, line):
     terms = [int(x) for x in line.strip().split(" ")]
-    if recursive(terms, 0, 0, num): return True
+    return recursive(terms, 0, 0, num)
 
 def recursive(terms, index, current, target):
     if index == len(terms): return current == target
+    if current > target: return False # slight optimization by pruning
     return (recursive(terms, index+1, current + terms[index], target) or 
             recursive(terms, index + 1, current * terms[index], target))
 
-partOne()
+def partTwo():
+    print(sum(int(line.split(":")[0]) for line in lines if check2(int(line.split(":")[0]), line.split(":")[1])))
+
+def check2(num, line):
+    terms = line.strip().split(" ")
+    return (recursive2(terms, 0, 0, num, "+") or 
+            recursive2(terms, 0, 0, num, "*") or
+            recursive2(terms, 0, 0, num, "||"))
+
+def recursive2(terms, index, current, target, op):
+    if index == len(terms): return current == target
+    if current > target: return False # slight optimization by pruning
+    if op == "+": current = current + int(terms[index])
+    if op == "*": current = current * int(terms[index])
+    if op == "||": current = int(str(current) + terms[index])
+    return (recursive2(terms, index + 1, current, target, "+") or 
+            recursive2(terms, index + 1, current, target, "*") or
+            recursive2(terms, index + 1, current, target, "||"))
+
+partOne() # O(2^n) :|
+partTwo() # O(3^n) :(
