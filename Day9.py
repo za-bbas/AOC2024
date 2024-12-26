2d54f1b151d84c525fecf9ef163fef366afea6dc
Zaid Abbas <zaid.sarmad2006@gmail.com>
2024-12-25 23:14:43 -0800

    part 2 pseudo code

--- Day9.py
+++ Day9.py
@@ -1,24 +1,18 @@
 with open("input9.txt", "r") as f:
     line = f.readline().strip()
-# check = [0,0,9,9,8,1,1,1,8,8,8,2,7,7,7,3,3,3,6,4,4,6,5,5,5,5,6,6]
-# runningSum = [0]
-# for i in range(1, len(check)):
-#     runningSum.append(runningSum[i-1] + check[i]*i)
-# print(runningSum)
 
 def partOne():
     disks = [int(line[i]) for i in range(len(line))]
-    total = 0
-    index = 0
+    total = index = 0
     i, j = 0, len(disks) - 1
     while i <= j:
         if i%2 == 0:
             for _ in range(disks[i]):
-                total += ((i//2)) * index
+                total += (i//2) * index
                 index += 1
         else:
             for _ in range(disks[i]):
-                total += ((j//2)) * index
+                total += (j//2) * index
                 disks[j] -= 1
                 if disks[j] == 0: j-=2
                 index += 1
@@ -26,4 +20,20 @@ def partOne():
         i += 1
     print(total)
 
+def partTwo():
+    """
+    Basic idea is simple:
+    iterate over file spans backwards (rtl), for each file span try to find leftmost big enough free span by iterating free spans forward (ltr).
+
+    possible optimizations:
+    1. Observation: if we couldn't find free span for file span of size N - it means that it won't be possible in future too
+    There are only 9 possible file span sizes (1-9), so it makes sense to maintain simple array of flags per size. 
+    If wee see in future file span of size N - we just skip it.
+    2.follows from first one - if all nine possible file span sizes are impossible to move - we are done with whole defragmentation and can bail out early.
+    3. Small optimization - we can calculate checksum of file span immediatelly after processing it.
+    Ie we don't need another dedicated loop iterating over file spans again after defragmentation.
+    """
+    # TODO: implement above idea
+
 partOne()
+partTwo()
