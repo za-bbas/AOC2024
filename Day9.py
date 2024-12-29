4f280975179448f4faa83be0d12feb9a2ee65203
Zaid Abbas <zaid.sarmad2006@gmail.com>
2024-12-28 17:30:41 -0800

    "more to worry about"

--- Day9.py
+++ Day9.py
@@ -23,17 +23,26 @@ def partOne():
 def partTwo():
     """
     Basic idea is simple:
-    iterate over file spans backwards (rtl), for each file span try to find leftmost big enough free span by iterating free spans forward (ltr).
+    iterate over files backwards (rtl), for each file try to find leftmost big enough free span by iterating spans forward (ltr).
 
     possible optimizations:
     1. Observation: if we couldn't find free span for file span of size N - it means that it won't be possible in future too
-    There are only 9 possible file span sizes (1-9), so it makes sense to maintain simple array of flags per size. 
+    There are only 9 possible file sizes (1-9), so it makes sense to maintain simple array of flags per size. 
     If wee see in future file span of size N - we just skip it.
-    2.follows from first one - if all nine possible file span sizes are impossible to move - we are done with whole defragmentation and can bail out early.
-    3. Small optimization - we can calculate checksum of file span immediatelly after processing it.
-    Ie we don't need another dedicated loop iterating over file spans again after defragmentation.
+    2.follows from first one: if all nine possible file sizes are impossible to move, we are done with whole defragmentation and can bail out early.
+    3. Small optimization: we can calculate checksum of file span immediatelly after processing it.
     """
     # TODO: implement above idea
+    # files = [int(line[i]) for i in range(0, len(line), 2)]
+    # spans = [int(line[i]) for i in range(1, len(line), 2)]
+    # for i in range(len(files)-1, 0, -1):
+    #     for j in range(len(spans)):
+    #         if spans[i]>=files[i]:
+    #             spans[i]-=files[i]
+    #             final 
+    # The above code wont work because it is neglecting that we can move files into a span that was already partially filled
+    # A way to solve this could be to keep track of possible indices and values, so that we cna just sort it in the end
+
 
 partOne()
 partTwo()
